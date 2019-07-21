b4de406ed5076577f9fe0eff72d2961c9036ab4f
rgunczer <rgunczer@gmail.com>
2019-07-21 21:36:15 +0200

    working store with redux

--- learning-react-2nd-chinnathambi/20reduxcounter/src/App.js
+++ learning-react-2nd-chinnathambi/20reduxcounter/src/App.js
@@ -1,9 +1,33 @@
-import React from 'react';
+import { connect } from 'react-redux';
+import Counter from './Counter';
 
-function App() {
-  return (
-    <p>App</p>
-  );
+// map redux state to component props
+function mapStateToProps(state) {
+  return {
+    countValue: state.count
+  };
 }
 
-export default App;
+// action
+var increaseAction = { type: 'increase' };
+var decreaseAction = { type: 'decrease' };
+
+// map redux actions to component props
+function mapDispatchToProps(dispatch) {
+  return {
+    increaseCount: function() {
+      return dispatch(increaseAction);
+    },
+    decreaseCount: function() {
+      return dispatch(decreaseAction);
+    }
+  }
+}
+
+// the HOC Higher Order Component
+var connectedComponent = connect(
+  mapStateToProps,
+  mapDispatchToProps
+)(Counter);
+
+export default connectedComponent;
--- learning-react-2nd-chinnathambi/20reduxcounter/src/index.js
+++ learning-react-2nd-chinnathambi/20reduxcounter/src/index.js
@@ -1,6 +1,19 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-
+import { createStore } from 'redux';
+import { Provider } from 'react-redux';
+import counter from './reducer';
 import App from './App';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+// store
+var store = createStore(
+    counter,
+    window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
+
+ReactDOM.render(
+    <Provider store={store}>
+        <App />
+    </Provider>,
+    document.getElementById('root')
+);
