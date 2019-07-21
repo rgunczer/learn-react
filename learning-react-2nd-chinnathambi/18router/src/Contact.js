1af2cf8a48ddb80d2b5b4bccf1dc405318c44107
rgunczer <rgunczer@gmail.com>
2019-07-21 20:41:44 +0200

    hashrouter sample

---
+++ learning-react-2nd-chinnathambi/18router/src/Contact.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Contact extends Component {
+    render() {
+        return (
+            <div>
+                <h2>CONTACT</h2>
+                <p>some text...</p>
+            </div>
+        );
+    }
+}
---
+++ learning-react-2nd-chinnathambi/18router/src/Home.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+export default class Home extends Component {
+    render() {
+        return (
+            <div>
+                <h2>HELLO</h2>
+                <p>some text...</p>
+            </div>
+        );
+    }
+}
