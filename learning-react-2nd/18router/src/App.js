2ee5e0c8ef6a9e64c1d74cf3853209feb436f82e
rgunczer <rgunczer@gmail.com>
2019-07-21 20:19:53 +0200

    update page h1 text

--- learning-react-2nd/18router/src/App.js
+++ learning-react-2nd/18router/src/App.js
@@ -12,7 +12,7 @@ function App() {
   return (
     <HashRouter>
       <div>
-        <h1>Simple SPA</h1>
+        <h1>Simple SPA with routing</h1>
         <ul className="header">
           <li><NavLink exact to="/">Home</NavLink></li>
           <li><NavLink to="/stuff">Stuff</NavLink></li>
