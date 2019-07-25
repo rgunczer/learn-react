c19512c4961755eb4590dcd58f7b06393599cd28
rgunczer <rgunczer@gmail.com>
2019-07-25 23:16:40 +0200

    update editor

--- pro-react-16/forms/src/Editor.js
+++ pro-react-16/forms/src/Editor.js
@@ -5,8 +5,12 @@ export class Editor extends Component {
     constructor(props) {
         super(props);
         this.state = {
-            name: ''
+            name: '',
+            flavor: 'Vanilla',
+            toppings: ['Strawberry']
         };
+        this.flavors = ['Chocolate', 'Double Chocolate', 'Triple Chocolate', 'Vanilla'];
+        this.toppings = ['Sprinkles', 'Fudge Sauce', 'Strawberries', 'Maple Syrup'];
     }
 
     updateFormValue = (event) => {
@@ -18,11 +22,31 @@ export class Editor extends Component {
         );
     }
 
+    updateFormValueOptions = (event) => {
+        let options = [...event.target.options]
+            .filter(o => o.selected).map(o => o.value);
+        this.setState({
+            [event.target.name]: options
+        }, () => this.props.submit(this.state))
+    }
+
     render() {
         return <div className="h5 bg-info text-white p-2">
             <div className="form-group">
                 <label>Name</label>
                 <input className="form-control" name="name" value={this.state.name} onChange={this.updateFormValue}></input>
+            </div>
+            <div className="form-group">
+                <label>Ice Cream Flavors</label>
+                <select className="form-control" name="flavor" value={this.state.flavor} onChange={this.updateFormValue}>
+                    { this.flavors.map(flavor => <option value={flavor} key={flavor}>{flavor}</option>)}
+                </select>
+            </div>
+            <div className="form-group">
+                <label>Ice Cream Toppings</label>
+                <select className="form-control" multiple={true} name="toppings" value={ this.state.toppings } onChange={ this.updateFormValueOptions }>
+                    { this.toppings.map(top => <option value={ top } key={ top }>{ top }</option>)}
+                </select>
             </div>
         </div>
     }
