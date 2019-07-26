d91b10df353cfc94be3337f4180da0994afdffcf
rgunczer <rgunczer@gmail.com>
2019-07-26 23:21:56 +0200

    form validation

--- pro-react-16/forms/src/Editor.js
+++ pro-react-16/forms/src/Editor.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { FormValidator } from './FormValidator';
+import { ValidationMessage } from './ValidationMessage';
 
 export class Editor extends Component {
 
@@ -8,20 +10,26 @@ export class Editor extends Component {
             name: '',
             flavor: 'Vanilla',
             twoScoops: false,
+            email: '',
             // toppings: ['Strawberry']
             toppings: ['Strawberries'],
             order: ''
         };
         this.flavors = ['Chocolate', 'Double Chocolate', 'Triple Chocolate', 'Vanilla'];
         this.toppings = ['Sprinkles', 'Fudge Sauce', 'Strawberries', 'Maple Syrup'];
+        this.rules = {
+            name: { required: true, minlength: 3, alpha: true },
+            email: { required: true, email: true },
+            order: { required: true }
+        }
     }
 
     updateFormValue = (event) => {
         this.setState(
             {
                 [event.target.name]: event.target.value
-            },
-            () => this.props.submit(this.state)
+            }
+            // () => this.props.submit(this.state)
         );
     }
 
@@ -53,6 +61,37 @@ export class Editor extends Component {
 
     render() {
         return <div className="h5 bg-info text-white p-2">
+            <FormValidator data={this.state} rules={this.rules} submit={this.props.submit}>
+                <div className="form-group">
+                    <label>Name</label>
+                    <input className="form-control"
+                        name="name"
+                        value={this.state.name}
+                        onChange={this.updateFormValue}/>
+                    <ValidationMessage field="name" />
+                </div>
+                <div className="form-group">
+                    <label>Email</label>
+                    <input className="form-control"
+                        naem="email"
+                        value={this.state.email}
+                        onChange={this.updateFormValue} />
+                    <ValidationMessage field="email" />
+                </div>
+                <div className="form-group">
+                    <label>Order</label>
+                    <textarea className="form-control"
+                        name="order"
+                        value={this.state.order}
+                        onChange={this.updateFormValue} />
+                    <ValidationMessage field="order" />
+                </div>
+            </FormValidator>
+        </div>
+    }
+
+    renderOld() {
+        return <div className="h5 bg-info text-white p-2">
             <div className="form-group">
                 <label>Name</label>
                 <input className="form-control" name="name" value={this.state.name} onChange={this.updateFormValue}></input>
@@ -72,12 +111,12 @@ export class Editor extends Component {
                                 type="radio" name="flavor"
                                 value={flavor}
                                 checked={this.state.flavor === flavor}
-                                onChange={this.updateFormValue}/>
+                                onChange={this.updateFormValue} />
                             <label className="form-check-label">
                                 {flavor}
                             </label>
                         </div>
-                        )
+                    )
                 }
             </div>
             {/* <div className="form-group">
@@ -88,16 +127,16 @@ export class Editor extends Component {
             </div> */}
             <div className="form-group">
                 <label>Ice Cream Toppsings</label>
-                { this.toppings.map(top =>
+                {this.toppings.map(top =>
                     <div className="form-check" key={top}>
                         <input className="form-check-input"
                             type="checkbox" name={top}
                             value={this.state[top]}
                             checked={this.state.toppings.indexOf(top) > -1}
-                            onChange={this.updateFormValueCheck}/>
-                            <label className="form-check-label">{top}</label>
+                            onChange={this.updateFormValueCheck} />
+                        <label className="form-check-label">{top}</label>
                     </div>
-                        )
+                )
                 }
             </div>
             <div className="form-group">
@@ -107,7 +146,7 @@ export class Editor extends Component {
                         name="twoScoops"
                         checked={this.state.twoScoops}
                         onChange={this.updateFormValueCheck} />
-                        <label className="form-check-label">Two Scoops</label>
+                    <label className="form-check-label">Two Scoops</label>
                 </div>
             </div>
             <div className="form-group">
