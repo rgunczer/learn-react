0ff21d82ef808ce76bdf7447285b650db5b28d80
rgunczer <rgunczer@gmail.com>
2019-07-28 22:07:39 +0200

    add misc files

--- misc/src/App.js
+++ misc/src/App.js
@@ -2,18 +2,29 @@ import React from 'react';
 import './App.css';
 import SimpleButton from './SimpleButton';
 import Greet from './Greet';
+import Dummy from './Dummy';
 
 class App extends React.Component {
   state = {
     buttonText: 'click',
-    buttonAge: 32
+    buttonAge: 32,
+    dummyText: 'some text'
   };
+
+  handleUpdate = () => {
+    this.setState({
+      dummyText: Math.random()
+    });
+  }
+
   render() {
     return (
       <>
         <p>App Here</p>
         <SimpleButton state={this.state} />
         <Greet name="Diana" heroName="Wonder Woman"/>
+        <Dummy text={this.state.dummyText}/>
+        <button onClick={this.handleUpdate}>Set</button>
       </>
     );
   }
