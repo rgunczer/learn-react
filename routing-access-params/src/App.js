cb6212a38cd391532128feea5834da5a8ee72a63
rgunczer <rgunczer@gmail.com>
2019-07-29 23:20:10 +0200

    initial checkin with not found route

---
+++ routing-access-params/src/App.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import { BrowserRouter as Router, Route, Link, Switch } from 'react-router-dom';
+
+import Home from './Home';
+import About from './About';
+import Person from './Person';
+import NotFound from './NotFound';
+
+function App() {
+  return (
+    <Router>
+      <div>
+        <p>App title here</p>
+      </div>
+      <ul>
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/person">Person</Link></li>
+      </ul>
+      <Switch>
+        <Route exact path="/" component={Home}/>
+        <Route path="/about" component={About}/>
+        <Route path="/person" component={Person}/>
+        <Route component={NotFound} />
+      </Switch>
+
+    </Router>
+  );
+}
+
+export default App;
---
+++ routing-access-params/src/Person.js
@@ -0,0 +1,8 @@
+import React, { Component } from 'react';
+
+export default class Person extends Component {
+
+    render() {
+        return <h2>Person here</h2>;
+    }
+}
