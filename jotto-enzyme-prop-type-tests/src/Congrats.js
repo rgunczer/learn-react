24b88fcab709746594cb3fb762a723c1d59e2461
rgunczer <rgunczer@gmail.com>
2020-02-25 23:08:30 +0100

    prop types tests

---
+++ jotto-enzyme-prop-type-tests/src/Congrats.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+/**
+ * Functional react component for congratulatory message
+ * @function
+ * @returns {JSX.Element} - Rendered component (or null if 'success' prop)
+ */
+const Congrats = (props) => {
+
+    if (props.success) {
+        return (
+            <div data-test="component-congrats">
+                <span data-test="congrats-message">
+                    Congratulations! You guessed the word!
+                </span>
+            </div>
+        );
+    } else {
+        return (
+            <div data-test="component-congrats"></div>
+        )
+    }
+
+};
+
+Congrats.propTypes = {
+    success: PropTypes.bool.isRequired
+};
+
+export default Congrats;
