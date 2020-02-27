ba9c6899792defe9ab71c58cbea4d18836463216
rgunczer <rgunczer@gmail.com>
2020-02-27 18:24:50 +0100

    non zero guess tests

--- jotto-enzyme-prop-type-tests/src/GuessedWords.js
+++ jotto-enzyme-prop-type-tests/src/GuessedWords.js
@@ -10,6 +10,31 @@ const GuessedWords = (props) => {
                 Try to guess the secret word
             </span>
         )
+    } else {
+        const guessedWordsRows = props.guessedWords.map((word, index) => {
+            return (
+                <tr key={index} data-test="guessed-word">
+                    <td>{word.guessedWord}</td>
+                    <td>{word.letterMatchCount}</td>
+                </tr>
+            )
+        });
+        contents = (
+            <div data-test="guessed-words">
+                <h3>Guessed Words</h3>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Guess</th>
+                            <th>Mathing Letters</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        { guessedWordsRows }
+                    </tbody>
+                </table>
+            </div>
+        )
     }
     return (
         <div data-test="component-guessed-words">
