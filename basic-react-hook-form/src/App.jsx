63ff7929b09fe06d0d02f77c71a1f7c2460c7c9b
Robert Gunczer <rgunczer@gmail.com>
2025-03-13 05:12:34 +0100

    basic hook form

---
+++ basic-react-hook-form/src/App.jsx
@@ -0,0 +1,40 @@
+import { useForm } from 'react-hook-form';
+import './App.css'
+
+function App() {
+  const {
+    register,
+    handleSubmit,
+    formState: { errors }
+  } = useForm({
+    defaultValues: {
+      'firstName': 'sarah',
+      'lastName': 'karrigan',
+    },
+  });
+
+  const onSubmit = (data) => {
+    console.log(data);
+  }
+
+  return (
+    <>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <div>
+          <input {...register("firstName")} />
+        </div>
+        <div>
+          <input {...register("lastName", { required: true })} />
+        </div>
+        <div>
+          {errors.lastName && <span>This field is required</span>}
+        </div>
+        <div>
+          <input type="submit" />
+        </div>
+      </form>
+    </>
+  )
+}
+
+export default App
