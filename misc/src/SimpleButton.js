f42753d1c085510b8ce57b1d408d13dc1474851d
Robert Gunczer <rgunczer@gmail.com>
2025-03-24 04:43:51 +0100

    remove old project

--- misc/src/SimpleButton.js
+++
@@ -1,9 +0,0 @@
-import React, { Component } from 'react';
-
-export default class SimpleButton extends Component {
-
-    render() {
-        return <p>SimpleButton here [{this.props.state.buttonText}] {this.props.state.buttonAge}</p>;
-    }
-
-}
