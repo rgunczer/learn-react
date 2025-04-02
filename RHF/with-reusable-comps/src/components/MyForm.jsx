bd5b31cde8993ea11a76e26f66730d67d508ceef
Robert Gunczer <rgunczer@gmail.com>
2025-04-02 05:53:21 +0200

    basic form with submit

--- RHF/with-reusable-comps/src/components/MyForm.jsx
+++ RHF/with-reusable-comps/src/components/MyForm.jsx
@@ -1,7 +1,19 @@
 import React from 'react'
+import { useForm } from 'react-hook-form';
 
 export const MyForm = () => {
+  const { register, handleSubmit } = useForm({});
+
+  const onSubmit = formData => {
+    console.log('onSubmit -> formData', formData);
+  }
+
   return (
-    <div>MyForm</div>
+    <div>
+      <form onSubmit={handleSubmit(onSubmit)}>
+        <input type="text" { ... register('name') } />
+        <button type="submit">Submit</button>
+      </form>
+    </div>
   )
 }
