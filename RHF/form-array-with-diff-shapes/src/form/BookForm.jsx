4b0d816cb010fd6f35f4a6cd9977cfbb341797dd
Robert Gunczer <rgunczer@gmail.com>
2025-05-10 06:53:31 +0200

    add some basic validation required, minLength

--- RHF/form-array-with-diff-shapes/src/form/BookForm.jsx
+++ RHF/form-array-with-diff-shapes/src/form/BookForm.jsx
@@ -5,17 +5,39 @@ export default function BookForm() {
   const { register, handleSubmit } = useForm();
 
   const onSubmit = formData => {
+    console.log('Succes: ', formData)
     alert(
       JSON.stringify(formData)
     );
   }
 
+  const onError = err => {
+    console.log('error', err);
+
+    for (const property in err) {
+      console.log('property: ',property);
+      console.log('type: ', err[property].type)
+      console.log('message: ', err[property].message)
+    }
+  }
+
   return (
     <div>
-      <form onSubmit={handleSubmit(onSubmit)}>
+      <form onSubmit={handleSubmit(onSubmit, onError)}>
         <input
           type="text"
-          {...register('title')}
+          {
+            ...register(
+              'title',
+              {
+                required: 'Field is required',
+                minLength: {
+                  value: 3,
+                  message: 'Too short'
+                }
+              }
+            )
+          }
         />
         <button>Submit</button>
       </form>
